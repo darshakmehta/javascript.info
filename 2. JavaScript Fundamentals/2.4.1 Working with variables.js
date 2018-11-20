f64f26061d123a7d50e2b5d11c64d149f04a655e
Mehta <dmehta9@uncc.edu>
2018-11-20 11:37:02 -0500

    JavaScript Fundamentals

---
+++ 2. JavaScript Fundamentals/2.4.1 Working with variables.js
@@ -0,0 +1,11 @@
+const name = "John";
+const admin = name;
+alert(admin);
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert( admin ); // "John"
---
+++ 2. JavaScript Fundamentals/2.4.2 Giving the right name.js
@@ -0,0 +1,6 @@
+const planetEarth;
+const currentUser;
+//Better
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+/*Modern editors and autocomplete make long variable names easy to write. Don’t save on them. A name with 3 words in it is fine.*/
